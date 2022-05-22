a446cb96b788228304d24e8329f0197ce1ee6fe2
Ryan Barrera <rbarrera9er@gmail.com>
2022-05-22 15:13:48 -0400

    commit tree 717ccb70ad7cd8e1b887718ae344c50c4a9192c0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+   // const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    console.log(drivers.slice(0, 2));
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(drivers){
+   // const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    console.log(drivers.slice(2, 5));
+    return drivers.slice(2, 5);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (fareMultiplier){
+    return function(fare){
+        return fareMultiplier * fare;
+    }
+}
+const fareDoubler = function(fare){
+    return fare * 2
+}
+const fareTripler = function(fare){
+    return fare * 3
+}          
+function selectDifferentDrivers(arrayOfDrivers, driverFunction){
+    return driverFunction(arrayOfDrivers)
+}
